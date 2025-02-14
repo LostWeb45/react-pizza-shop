0692a10e71e4c19f4fb0c62b479afe9e07e38df7
LostWeb45 <kostik052005@gmail.com>
2025-02-14 17:33:13 +0300

    Вывод пицц

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
+import pizzas from "./assets/pizzas.json";
+
 function App() {
   return (
     <div className="wrapper">
@@ -17,7 +19,15 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock />
+            {pizzas.map((obj) => (
+              <PizzaBlock
+                key={obj.title}
+                title={obj.title}
+                price={obj.price}
+                imageUrl={obj.imageUrl}
+                sizes={obj.sizes}
+              />
+            ))}
           </div>
         </div>
       </div>
