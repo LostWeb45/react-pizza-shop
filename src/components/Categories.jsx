be79a284cdf0894ab8a7041b80d3ef12048b8797
LostWeb45 <kostik052005@gmail.com>
2025-02-14 16:54:15 +0300

    Перенос картинок в assets

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,50 @@
 import React from "react";
 
 const Categories = () => {
+  const [activeCategory, setActiveCategory] = React.useState(0);
+
+  const onClickCategory = () => {
+    setActiveCategory(1);
+  };
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        <li
+          onClick={() => onClickCategory()}
+          className={activeCategory === 0 ? "active" : ""}
+        >
+          Все
+        </li>
+        <li
+          onClick={onClickCategory}
+          className={activeCategory === 1 ? "active" : ""}
+        >
+          Мясные
+        </li>
+        <li
+          onClick={onClickCategory}
+          className={activeCategory === 2 ? "active" : ""}
+        >
+          Вегетарианская
+        </li>
+        <li
+          onClick={onClickCategory}
+          className={activeCategory === 3 ? "active" : ""}
+        >
+          Гриль
+        </li>
+        <li
+          onClick={onClickCategory}
+          className={activeCategory === 4 ? "active" : ""}
+        >
+          Острые
+        </li>
+        <li
+          onClick={onClickCategory}
+          className={activeCategory === 5 ? "active" : ""}
+        >
+          Закрытые
+        </li>
       </ul>
     </div>
   );
