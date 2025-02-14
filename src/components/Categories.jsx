eb9891bdab8e627f4e024decbaed3ba13b23c05c
LostWeb45 <kostik052005@gmail.com>
2025-02-14 17:00:44 +0300

    Выбор категории

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,44 +3,44 @@ import React from "react";
 const Categories = () => {
   const [activeCategory, setActiveCategory] = React.useState(0);
 
-  const onClickCategory = () => {
-    setActiveCategory(1);
+  const onClickCategory = (index) => {
+    setActiveCategory(index);
   };
   return (
     <div className="categories">
       <ul>
         <li
-          onClick={() => onClickCategory()}
+          onClick={() => onClickCategory(0)}
           className={activeCategory === 0 ? "active" : ""}
         >
           Все
         </li>
         <li
-          onClick={onClickCategory}
+          onClick={() => onClickCategory(1)}
           className={activeCategory === 1 ? "active" : ""}
         >
           Мясные
         </li>
         <li
-          onClick={onClickCategory}
+          onClick={() => onClickCategory(2)}
           className={activeCategory === 2 ? "active" : ""}
         >
           Вегетарианская
         </li>
         <li
-          onClick={onClickCategory}
+          onClick={() => onClickCategory(3)}
           className={activeCategory === 3 ? "active" : ""}
         >
           Гриль
         </li>
         <li
-          onClick={onClickCategory}
+          onClick={() => onClickCategory(4)}
           className={activeCategory === 4 ? "active" : ""}
         >
           Острые
         </li>
         <li
-          onClick={onClickCategory}
+          onClick={() => onClickCategory(5)}
           className={activeCategory === 5 ? "active" : ""}
         >
           Закрытые
