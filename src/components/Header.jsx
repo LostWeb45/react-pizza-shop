a62f26e5a9df8d39f65c9f4a31d66092906b1acc
LostWeb45 <kostik052005@gmail.com>
2025-02-26 12:53:08 +0300

    Инпут с удалением содержимого по крестику

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import pizzaLogo from "../assets/img/pizza-logo.svg";
 import { Link } from "react-router-dom";
 import Search from "./Search";
-const Header = () => {
+const Header = ({ searchValue, setSearchValue }) => {
   return (
     <div className="header">
       <div className="container">
@@ -15,7 +15,7 @@ const Header = () => {
             </div>
           </div>
         </Link>
-        <Search />
+        <Search searchValue={searchValue} setSearchValue={setSearchValue} />
 
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
