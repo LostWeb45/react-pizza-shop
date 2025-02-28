7d412219f0fe08db7c5d9850ccab657b11ea5595
LostWeb45 <kostik052005@gmail.com>
2025-02-28 15:49:55 +0300

    Сортировка через Redux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { use } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
+import { setCategoryId } from "../redux/slices/filterSlice";
 import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock";
@@ -9,15 +11,22 @@ import { SearchContext } from "../App";
 // import Search from "../components/Search";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const categoryId = useSelector((state) => state.filter.categoryId);
+
   const { searchValue } = React.useContext(SearchContext);
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
-  const [categoryId, setCategoryId] = React.useState(0);
+  // const [categoryId, setCategoryId] = React.useState(0);
   const [currentPage, setCurrentPage] = React.useState(1);
   const [sortType, setSort] = React.useState({
     name: "популярности ↓",
     sortProperty: "rating",
   });
+
+  const onClickCategory = (id) => {
+    dispatch(setCategoryId(id));
+  };
 
   React.useEffect(() => {
     setIsLoading(true);
@@ -55,10 +64,7 @@ const Home = () => {
   return (
     <div className="container">
       <div className="content__top">
-        <Categories
-          value={categoryId}
-          onClickCategory={(index) => setCategoryId(index)}
-        />
+        <Categories value={categoryId} onClickCategory={onClickCategory} />
         <Sort value={sortType} onChangeSort={(index) => setSort(index)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -1,23 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const counterSlice = createSlice({
-  name: "counter",
-  initialState: {
-    value: 0,
+const initialState = {
+  categoryId: 0,
+  sort: {
+    name: "популярности ↓",
+    sortProperty: "rating",
   },
+};
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    setCategoryId(state, action) {
+      console.log(action);
+      state.categoryId = action.payload;
     },
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { setCategoryId } = filterSlice.actions;
 
-export default counterSlice.reducer;
+export default filterSlice.reducer;
