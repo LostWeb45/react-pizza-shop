4ab682d45c7e1184b20c0a4bb49fa85029c66822
LostWeb45 <kostik052005@gmail.com>
2025-03-10 15:39:54 +0300

    Внутренняя странца для пиццы

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./scss/app.scss";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import FullPizza from "./pages/FullPizza";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/pizza/:id" element={<FullPizza />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
---
+++ src/pages/FullPizza.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FullPizza = () => {
+  return (
+    <div className="container">
+      <img src="" />
+      <h2>Название пиццы</h2>
+      <p>Описание состава пиццы</p>
+    </div>
+  );
+};
+
+export default FullPizza;
