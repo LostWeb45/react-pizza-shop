6ea6ed3664f3c6f3b086c30d24a5472f9e3d6c9e
LostWeb45 <kostik052005@gmail.com>
2025-03-10 20:28:24 +0300

    Запрос на полученеи данныз о пицце

--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const FullPizza = () => {
+  const [pizza, setPizza] = React.useState();
+  const { id } = useParams();
+
+  React.useEffect(() => {
+    async function fetchPizza() {
+      try {
+        const { data } = await axios.get(
+          `https://67b2f30ebc0165def8cf45b6.mockapi.io/items/` + id
+        );
+        setPizza(data);
+      } catch (err) {
+        console.error("Ошибка при открытие пиццы " + err);
+      }
+    }
+    fetchPizza();
+  }, []);
+
   return (
     <div className="container">
-      <img src="" />
+      {/* <img src={pizza.imageUrl} /> */}
       <h2>Название пиццы</h2>
       <p>Описание состава пиццы</p>
     </div>
