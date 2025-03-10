a870e30010b963cf5d58ff7b411771a0e77567e3
LostWeb45 <kostik052005@gmail.com>
2025-03-10 21:01:09 +0300

    Вынос главного лаяута

--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const FullPizza = () => {
   const [pizza, setPizza] = React.useState({});
   const { id } = useParams();
+  const navigate = useNavigate();
 
   React.useEffect(() => {
     async function fetchPizza() {
@@ -15,6 +16,7 @@ const FullPizza = () => {
         setPizza(data);
       } catch (err) {
         console.error("Ошибка при открытие пиццы " + err);
+        navigate("/");
       }
     }
     fetchPizza();
