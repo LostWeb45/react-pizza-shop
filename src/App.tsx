d3511e8ce9a76e607210b4059d85f86e39af9e74
LostWeb45 <kostik052005@gmail.com>
2025-03-18 19:12:14 +0300

    Lazy load для корзины

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import "./scss/app.scss";
 import Home from "./pages/Home";
 import FullPizza from "./pages/FullPizza";
-import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+
+const Cart = React.lazy(() => import("./pages/Cart"));
+// Элемент подключается, только когда он будет отрисовываться
 
 function App() {
   return (
